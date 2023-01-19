f6000ba7ce7f694cf4c737aeea184c637a98e44e
unnikalpalam <arungkm@gmail.com>
2023-01-19 15:45:31 +0530

    google sheet submission added

--- mail/contact.js
+++ mail/contact.js
@@ -1,4 +1,3 @@
-
 var btn = document.getElementById('send');
 btn.addEventListener('click', function (e) {
     e.preventDefault()
@@ -6,23 +5,20 @@ btn.addEventListener('click', function (e) {
     var email = document.getElementById('email').value;
     var subject = document.getElementById('subject').value;
     var text = document.getElementById('message').value;
-    var body = 'Name : ' + name + '<br/>Email : ' + email + '<br/> Subject : ' + subject + '<br/> Message :' + text;
     if (name=="" || email=="" || subject=="" || text==""){
        
         document.getElementById("err1").innerHTML ='Please enter details';
         document.getElementById('err2').innerHTML="" ;
     }
     else{
-    Email.send({
-        SecureToken : "dfeefa81-12c0-4b00-b730-55e35fdc5e0a",
-        To: 'arungkm@gmail.com',
-        From: 'verifyhostuser@gmail.com',
-        Subject: 'A message from user',
-        Body: body
-    }).then(document.getElementById("err2").innerHTML ='success, i will contact you!');
-    document.getElementById('formsub').reset();
-    document.getElementById('err1').innerHTML="";
-}
+        const scriptURL = 'https://script.google.com/macros/s/AKfycbzKg8JdI6L10hXLnz-gWPCdoN__LRH3gLp5lB1Db9Z5AX4Z3cnJ_5K0nwHir7YmFlL7Ag/exec'
+        const form = document.forms['formsub']
+        e.preventDefault()
+        fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+        .then(document.getElementById("err2").innerHTML ='success, i will contact you!');
+        document.getElementById('formsub').reset();
+        document.getElementById('err1').innerHTML="";
+    }
 })
 
 
